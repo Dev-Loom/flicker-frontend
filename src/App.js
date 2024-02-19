91c333ff02cf79d33d2444d1f31ca9ccba46a6cd
Sujit Kumar <workmail.sujit@gmail.com>
2024-02-19 23:34:16 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Navbar from "./Components/Navbar/Navbar";
-
+import Footer from "./Components/Footer/Footer";
+import "./App.css";
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
+      <a href="https://wa.me/917019693927" class="whatsapp_float" target="_blank" rel="noopener noreferrer">
+        <i class="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
                 href="https://google.com"
                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Faq
+                F.A.Q
               </a>
             </li>
             <li>
